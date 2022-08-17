d5821edcccb52d6287a771257dc311e41bd1660e
lyndabelfar <lyndabelar@gmail.com>
2022-08-17 13:11:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [temperature, setTemperature] = useState(10);
+  const [tempColor, setTempColor] = useState('cold')
+
+
+  const increaseTemp =()=>{
+    if(temperature >= 30) return 30;
+
+    let newTemp = temperature+1
+    setTemperature(newTemp);
+
+    if(newTemp>=15){
+      setTempColor('hot')
+    }else{
+      setTempColor('cold')
+
+    }
+  }
+  const decreaseTemp =()=>{
+    if(temperature <= 0) return 0;
+
+    let newTemp = temperature-1
+
+    setTemperature(newTemp);
+    if(newTemp>=15){
+      setTempColor('hot')
+    }else{
+      setTempColor('cold')
+
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className={`temp-circle ${tempColor}`}>
+        <div className="temp">{temperature}°C</div>
+      </div>
+      <div className="btns">
+        <button onClick={increaseTemp}>+</button>
+        <button onClick={decreaseTemp}>-</button>
+      </div>
     </div>
   );
 }
